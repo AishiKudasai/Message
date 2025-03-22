51543ee90bc1c11ebf47218765b658aef319dbda
AishiKudasai <145766143+AishiKudasai@users.noreply.github.com>
2025-03-22 17:38:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('Flowers For You!').split('')
+      const titles = ('Qpall').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
