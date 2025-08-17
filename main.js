4771dbc120c824d521b9ef2af7ec092b12a5bb61
AishiKudasai <145766143+AishiKudasai@users.noreply.github.com>
2025-08-17 18:32:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('Qpallll').split('')
+      const titles = ('<3').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
@@ -19,4 +19,5 @@ onload = () => {
   
       clearTimeout(c);
     }, 1000);
+
   };
